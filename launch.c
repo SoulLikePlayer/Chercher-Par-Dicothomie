e01a71f092ae37b1392f8d7ec148d10373168c02
louis Lazare <louislazare.pro@gmail.com>
2024-10-09 10:45:09 +0200

    [INITIAL COMMIT]

---
+++ launch.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+
+void launch(){
+    printf("=====Bienvenue dans le jeu !====");
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "launch.c"
+
+void launch();
+
+int main(int argc, char *argv[]){
+    launch();
+    return 0;
+}
